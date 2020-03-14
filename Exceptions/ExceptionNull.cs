34d88297629f05f2dc6df81ebcfb22266a2a2f4e
rightbil <rightbil@gmail.com>
2020-03-14 16:34:46 -0400

    Projects add , classes changed, Nuget updated

---
+++ Exceptions/ExceptionNull.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exceptions
+{
+  public class ExceptionNull
+    {
+    }
+}
--- Models/Teacher.cs
+++ Models/Teacher.cs
@@ -1,35 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-
 namespace Models
 {
-
     public class Teacher
     {
         /*[HiddenInput(DisplayValue=false)]*/
         public int ID { get; set; }
-
         [DisplayName("Last Name")]
         [Required(ErrorMessage = "Last Name is required")]
         public string LastName { get; set; }
         [DisplayName("First Name")]
         [Required(ErrorMessage = "First Name is required")]
         public string FirstName { get; set; }
-        [DisplayName("Enrollment Date")]
-        [Required(ErrorMessage = "Enrollment Date is required")]
-        public DateTime EnrollmentDate { get; set; }
-        
+        public DateTime EnrollmentDate { get; private set; } = DateTime.Now;
         [Required(ErrorMessage = "Major is required")]
         public String Major { get; set; }
-        public Course courses { get; set; }
+        public Course assignedCourses { get; set; }
+        
         /*public virtual ISet<Course> xcourses { get; set; }
         public virtual IList<Course> ycourses { get; set; }*/
+        
+        // public Teacher()
+        // {
+        //     EnrollmentDate = DateTime.Now;
+        // }
+        // public Teacher(string firstName, string lastName, String major, Course assignedCourses)
+        // {
+        //     this.FirstName = firstName;
+        //     this.LastName = lastName;
+        //     this.Major = major;
+        //     this.assignedCourses = assignedCourses;
+        //     this.EnrollmentDate = DateTime.Now;
+        //     
+        // }
 
     }
 }
